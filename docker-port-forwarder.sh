b1c95c1807db73137f3214a8eb1e7c54887254a6
Max von Webel <max@343max.de>
2024-02-29 13:01:00 +0100

    different local port and container port

--- docker-port-forwarder.sh
+++ docker-port-forwarder.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Check if exactly 3 arguments are provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <host> <container> <port>"
-    exit 1
+if [ "$#" -lt 3 ]; then
+  echo "Usage: $0 <host> <container> <port> [local_port]"
+  exit 1
 fi
 
 # Assign arguments to variables for better readability
 host=$1
 container=$2
 port=$3
+local_port=${4:-$port}
 
 # SSH into the host and run docker inspect on the container
 container_ip=$(ssh $host "docker inspect $container" | jq -r '.[0].NetworkSettings.Networks[].IPAddress' | head -n 1)
 
 echo "Forwarding port $port from $container ($container_ip) on $host to localhost..."
 
-ssh -L localhost:$port:$container_ip:$port $host 'echo "Press Ctrl+C to stop forwarding" && sleep 365d'
+ssh -L localhost:$local_port:$container_ip:$port $host 'echo "Press Ctrl+C to stop forwarding" && sleep 365d'
